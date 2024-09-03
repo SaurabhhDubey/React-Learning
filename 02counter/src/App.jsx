66c287bb54a8c11925cc177450e9ee4752522215
Saurabh Dubey <saurabhdubey3712@gmail.com>
2024-09-03 18:40:59 +0530

    hooks

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,11 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-   
+
+  const[counter , setCounter]  useState(15)
+   //let counter = 15
+
+
+   const addValue = () => {
+    console.log("clicked" , Math.random());
+    counter = counter + 1
+   }
 
   return (
     <>
      <h1>chai aur react</h1>
+     <h2>Counter value: {counter}</h2>
+    
+    <button onClick={addValue} >Add value</button>
+    <br></br>
+    <button >remove value</button>
     </>
   )
 }
