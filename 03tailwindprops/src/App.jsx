b07babc1ba3604eadc28ee487dc7deb6338ab2fb
Saurabh Dubey <saurabhdubey3712@gmail.com>
2024-09-10 16:17:11 +0530

    props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -6,10 +6,15 @@ import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj = {
+    username : "Saurabh",
+    age : 20
+  }
 
   return (
     <>
      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwent Test</h1>
+     <Card channel="chaiaurcode" someObje={myObj}/>
     <Card/>
     </> 
   )
