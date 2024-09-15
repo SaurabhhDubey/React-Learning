463b57e08002240467755f0cd603747be2a1bf92
Saurabh Dubey <saurabhdubey3712@gmail.com>
2024-09-15 13:42:57 +0530

    update

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -13,10 +13,17 @@ function App() {
     let str  = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     if (numberAllowed) str += "0123456789"
     if (charAllowed) str += "~!@#$%&:;/"
+    for(let i = 1; i<=array.length ; i++){
+      let char = Math.floor( Math.random() * str.length +1)
+      pass = str.charAt(char)
+    }
+
+    setPassword(pass)
+
   } , [length , numberAllowed , charAllowed , setPassword])
   return (
     <>
-    <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+   <div className='w-full max-w-md max-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700'></div>
     </>
   )
 }
